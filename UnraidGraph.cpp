13c100d0350f80aeaf291e891d847707282b9e26
jnex26 <rob@howeonline.org>
2025-07-27 01:27:02 +0100

    Fixed Clash with an existing Library.
    twas my own library but changing it elsewhere would have been pain full

--- UnraidGraph.cpp
+++ UnraidGraph.cpp
@@ -2,13 +2,23 @@
 #include "UnraidGraph.h"
      
 String apikey; // the Auth Creds for the APi Key 
-String unraidapi; // the url for the unraid API 
+String unraidapiurl; // the url for the unraid API 
+bool debugMode = false; // default debugmode 
 
 
 
 // constructor 
 UnraidGraph::UnraidGraph()
 {
+}
+
+void UnraidGraph::begin(String graphurl,String key){
+  apikey = key; 
+  unraidapiurl = graphurl;
+}
+
+void UnraidGraph::debug(bool mode){
+  debugMode = mode; 
 }
 
 DynamicJsonDocument UnraidGraph::getGraph(DynamicJsonDocument __GraphQuery){  
@@ -20,11 +30,11 @@ DynamicJsonDocument UnraidGraph::getGraph(DynamicJsonDocument __GraphQuery){
   __doc = false;
   WiFiClient client;
   HTTPClient http;
-  http.begin(client,unraidapi);
+  http.begin(client,unraidapiurl);
   http.setTimeout(5000);
   http.addHeader("Content-Type", "application/json");
   http.addHeader("x-api-key", apikey);
-  Serial.println(__jsonWebcall);
+  if (debugMode) {Serial.println(__jsonWebcall);}
   http.POST(__jsonWebcall);
   deserializeJson(__doc, http.getString());
   http.end();
@@ -40,8 +50,14 @@ DynamicJsonDocument UnraidGraph::returnGraphQuery(DynamicJsonDocument __query) {
   __temp.replace(F(":"),"");
   __temp.replace("\"","");
   __temp = __temp.substring(1,__temp.length()-1);
+
   DynamicJsonDocument __tempdoc(2036);
   __tempdoc["query"] = __temp;
+  if (debugMode) {
+    String __reallytemp; 
+    serializeJson(__tempdoc,__reallytemp);
+    Serial.println(__reallytemp);
+  }
   return __tempdoc;
 }
 
@@ -52,6 +68,16 @@ DynamicJsonDocument UnraidGraph::getUnraidParityHistory() {
   for(String __id: __ids){
     __query["query"]["parityHistory"][__id]=true;
   }
+  if (debugMode) {
+    String __reallytemp; 
+    serializeJson(__query,__reallytemp);
+    Serial.println(__reallytemp);
+  }
   __resultset = UnraidGraph::getGraph(__query);
+    if (debugMode) {
+    String __reallytemp; 
+    serializeJson(__resultset,__reallytemp);
+    Serial.println(__reallytemp);
+  }
    return __resultset; 
 }
